64b301ab73db3e1544cf3709d2dbbe1b99e0feef
annamariak02 <92162125+annamariak02@users.noreply.github.com>
2021-10-08 15:33:43 +0300

    Create checkTemp.js

---
+++ homework/checkTemp.js
@@ -0,0 +1,14 @@
+function checkTemp(temp){
+    if (temp<=20)
+    {
+        return -1
+    }
+    else if(checkTemp>=21 && checkTemp<=40)
+    {
+       return 0   
+    }
+    else 
+    {
+       return 1
+    }
+}
